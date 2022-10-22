ed39470789c2837c42c8e85b2ee62e7ec4332522
Adrian Cheung <adrian.p.cheung@gmail.com>
2022-10-22 01:25:46 -0400

    add dimensions to player

--- src/Player.h
+++ src/Player.h
@@ -6,15 +6,20 @@
 
 class Player {
 public:
-	Vec2 pos;
+    Vec2 pos;
+    Vec2 dimens;
     float angle = 0.0f;
 
-    explicit Player(Vec2 pos) : pos(pos) {}
+    explicit Player(Vec2 pos) : pos(pos) {
+        dimens = {{texture.width * scale, texture.height * scale}};
+    }
 
     void Update(float deltaTime);
-	void Render();
+
+    void Render();
+
 private:
-	RayTexture texture = { Paths::Image("HoverCraft") };
+    RayTexture texture = {Paths::Image("HoverCraft")};
     float scale = 25.0f;
 };
 
