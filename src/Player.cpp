2a137b30b51f3aa5ac6b25e42be06b4c3d5fd79e
Adrian Cheung <adrian.p.cheung@gmail.com>
2022-10-22 12:43:50 -0400

    add thruster physics

--- src/Player.cpp
+++ src/Player.cpp
@@ -15,19 +15,49 @@ void Player::Update(float deltaTime) {
     float speed = 5.0f;
 
     if (IsKeyDown(KEY_W)) {
-        pos.y -= speed;
+        pos.y -= speed * deltaTime;
     }
     if (IsKeyDown(KEY_S)) {
-        pos.y += speed;
+        pos.y += speed * deltaTime;
     }
     if (IsKeyDown(KEY_A)) {
-        pos.x -= speed;
+        pos.x -= speed * deltaTime;
     }
     if (IsKeyDown(KEY_D)) {
-        pos.x += speed;
+        pos.x += speed * deltaTime;
     }
 
     float tiltSpeed = 5.0f;
-    if (IsKeyDown(KEY_LEFT)) { angle -= tiltSpeed; }
-    if (IsKeyDown(KEY_RIGHT)) { angle += tiltSpeed; }
+    if (IsKeyDown(KEY_LEFT)) { angle -= tiltSpeed * deltaTime; }
+    if (IsKeyDown(KEY_RIGHT)) { angle += tiltSpeed * deltaTime; }
+
+    if (IsKeyDown(KEY_Z)) {
+        Vec2 bottomLeft = {pos.x - dimens.x / 2.0f * cos(angle) - dimens.y / 2.0f * sin(angle),
+                           pos.y + dimens.y / 2.0f * cos(angle) - dimens.x / 2.0f * sin(angle)};
+        ApplyForce({20, -1}, bottomLeft, deltaTime);
+    }
+    if (IsKeyDown(KEY_X)) {
+        ApplyForce({20, -1}, {pos.x + dimens.x / 2.0f, pos.y}, deltaTime);
+    }
+
+}
+
+void Player::ApplyForce(Vec2 force, Vec2 point, float deltaTime) {
+    Vec2 r = pos - point;
+
+//    Vec2 torque = r.cross(force);
+    float torque = r.x * force.y - r.y * force.x;
+    float rotInertia = mass * (dimens.x * dimens.x + dimens.y * dimens.y) / 12.0f;
+
+    accel = force / mass;
+    vel += accel * deltaTime;
+    angularAccel += torque / rotInertia;
+    angularVel += angularAccel * deltaTime;
+    angle += angularVel * deltaTime;
+
+    pos += vel * deltaTime;
+}
+
+void Player::ApplyForceCenter(Vec2 force) {
+    accel = force / mass;
 }
--- src/Player.h
+++ src/Player.h
@@ -9,12 +9,28 @@ public:
     Vec2 pos;
     Vec2 dimens;
     float angle = 0.0f;
+//    float deltaTime = 1.0f;
+
+    Vec2 vel;
+    Vec2 accel;
+    float angularVel;
+    float angularAccel;
+
+    Vec2 force;
+    Vec2 forceLeft;
+    Vec2 forceRight;
+    float mass = 1.0f;
+
 
     explicit Player(Vec2 pos) : pos(pos) {
         dimens = {{texture.width * scale, texture.height * scale}};
     }
 
     void Update(float deltaTime);
+
+    void ApplyForce(Vec2 force, Vec2 point, float deltaTime);
+
+    void ApplyForceCenter(Vec2 force);
 
     void Render();
 
